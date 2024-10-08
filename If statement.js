4e0dcd01233e8c08ed2c52e9b9f620c3f348740f
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-10-08 13:21:50 +0800

    Add files via upload

---
+++ If statement.js
@@ -0,0 +1,14 @@
+//IF STATEMENT
+
+//let age = 17;
+//if (age <= 13) {
+//   console.log("Child");
+//} 
+
+//ELSE STATEMENT
+let age = 30;
+if (age == 13-19) {
+    console.log("You are a Teenager");
+} else {
+    console.log("You are an Adult");
+}
